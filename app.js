fca87515e96a55e936761306568c81a517adc6df
teamREFIT <105735247+NoahDyKim@users.noreply.github.com>
2022-06-13 10:23:53 +0900

    Saving and filling.
    Made fill and save buttons for the canvas.

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".jsColor");
 const range = document.querySelector("#jsRange");
 const mode = document.querySelector("#jsMode");
+const saveBtn = document.querySelector("#jsSave");
 
-canvas.width = 600;
-canvas.height = 600;
+const INITIAL_COLOR = "2c2c2c";
+const CANVAS_SIZE = 600;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = "INITIAL_COLOR";
+ctx.fillStyle = "INITIAL_COLOR";
 ctx.lineWidth = 2.5;
+
+
 
 let painting = false;
 let filling = false;
@@ -42,8 +51,12 @@ function onMouseUp() {
 }
 
 function handleColorClick(event) {
+//  border-style: solid;
+//  border-color: black;
+//  border-width: 10px;
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -61,11 +74,31 @@ if(filling === true) {
 }
 }
 
+function handleCanvasClick() {
+  if(filling) {
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "paintJs[😄]";
+  link.click();
+}
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -76,4 +109,8 @@ if(range) {
 
 if(mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
